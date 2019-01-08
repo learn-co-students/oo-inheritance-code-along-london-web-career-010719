6c812c9826bc008fc59816c84085279d5c7a67ff
github username <github email address>
2019-01-08 17:32:42 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,19 +1,7 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" # Car class need access to the Vehicle class(to the file)
 
-class Car
-
-  attr_accessor :wheel_size, :number, :wheel_number
-  
-  def initialize(wheel_size, number)
-    @wheel_size = wheel_size
-    @number = number
-  end
-
-  def go
+class Car < Vehicle
+  def go #overwrite(specific for Car class)
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-  end
-
-  def fill_up_tank
-    "filling up!"
   end
 end
